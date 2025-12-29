24bc5ba9e1d74bd98e26cdd5e902bfa881c8a0cf
vaibhav chaturvedi <vaibhavchaturvedi.work@gmail.com>
2025-12-29 22:44:49 +0530

    adding thread schema

---
+++ backend/models/Thread.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import MessageSchema from "./Message";
+
+const ThreadSchema = new mongoose.Schema({
+    threadId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    title: {
+        type: String,
+        default: "New Chat"
+    },
+    messages: [MessageSchema],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+export default mongoose.model("Thread", ThreadSchema);
+
