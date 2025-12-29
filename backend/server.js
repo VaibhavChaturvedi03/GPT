5ec552325c64ff7dc6968b08a66ea73cf4219044
vaibhav chaturvedi <vaibhavchaturvedi.work@gmail.com>
2025-12-29 23:16:15 +0530

    updating

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import "dotenv/config";
+import mongoose from "mongoose";
 import cors from 'cors';
 
 const app = express();
@@ -8,9 +9,21 @@ const PORT = 8080;
 app.use(express.json()); //to parse incoming req
 app.use(cors());
 
+const connectDB = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
+    }catch(err){
+        console.log("MongoDB failed to connect");
+        console.log(err);
+    }
+}
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
+    connectDB();
 });
+
 
 
 
