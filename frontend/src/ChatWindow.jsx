fe0dad2a4fcb5f842ea9c1fae2d37ec1432d2c23
vaibhav chaturvedi <vaibhavchaturvedi.work@gmail.com>
2025-12-31 02:08:28 +0530

    ChatWindow Component

---
+++ frontend/src/ChatWindow.jsx
@@ -0,0 +1,29 @@
+import './ChatWindow.css';
+import Chat from './Chat.jsx';
+
+function ChatWindow(){
+    return(
+        <div className="chatWindow">
+            <div className="navbar">
+                <span>GPT <i class="fa-solid fa-chevron-down"></i> </span>
+                <div className="userIconDiv">
+                    <span><i class="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+            <Chat></Chat> 
+            
+            <div className="chatInput">
+                <div className="userInput">
+                    <input placeholder='Ask Anything'>
+                    </input>
+                    <div id='submit'><i class="fa-solid fa-paper-plane"></i></div>
+                </div>
+                <p className="info">
+                    GPT can make mistakes. Check important info. See Cookie Preferences.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ChatWindow;
