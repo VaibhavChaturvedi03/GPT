1c6751c48c699dd323a8da6a81806c95056d5692
vaibhav chaturvedi <vaibhavchaturvedi.work@gmail.com>
2025-12-31 15:16:36 +0530

    getting reply from ai

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,22 +1,53 @@
 import './ChatWindow.css';
 import Chat from './Chat.jsx';
+import { MyContext } from './MyContext.jsx';
+import { useContext } from 'react';
 
-function ChatWindow(){
-    return(
+function ChatWindow() {
+
+    const { prompt, setPrompt, reply, setReply, currThreadId } = useContext(MyContext);
+
+    const getReply = async() => {
+        const options = {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                message: prompt,
+                threadId: currThreadId
+            })
+        };
+
+        try{
+            const response = await fetch('http://localhost:8080/api/chat', options);
+            const res = await response.json();
+            console.log(res);
+            setReply(res.reply);
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    return (
         <div className="chatWindow">
             <div className="navbar">
-                <span>GPT <i class="fa-solid fa-chevron-down"></i> </span>
+                <span>GPT <i className="fa-solid fa-chevron-down"></i> </span>
                 <div className="userIconDiv">
-                    <span className="userIcon"><i class="fa-solid fa-user"></i></span>
+                    <span className="userIcon"><i className="fa-solid fa-user"></i></span>
                 </div>
             </div>
-            <Chat></Chat> 
-            
+            <Chat></Chat>
+
             <div className="chatInput">
                 <div className="userInput">
-                    <input placeholder='Ask Anything'>
+                    <input placeholder='Ask Anything'
+                        value={prompt}
+                        onChange={(e) => setPrompt(e.target.value)}
+                        onKeyDown={(e) => e.key === "Enter"? getReply():''}
+                    >
                     </input>
-                    <div id='submit'><i class="fa-solid fa-paper-plane"></i></div>
+                    <div id='submit' onClick={getReply}><i className="fa-solid fa-paper-plane"></i></div>
                 </div>
                 <p className="info">
                     GPT can make mistakes. Check important info. See Cookie Preferences.
