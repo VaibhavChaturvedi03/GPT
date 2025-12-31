c0c3e03a9e753709feb5f85f548a885ae9017376
vaibhav chaturvedi <vaibhavchaturvedi.work@gmail.com>
2025-12-31 18:11:02 +0530

    updating post route

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -75,7 +75,7 @@ router.post('/chat', async (req, res) => {
     }
 
     try {
-        const thread = await Thread.findOne({ threadId });
+        let thread = await Thread.findOne({ threadId });
 
         if(!thread){
             //create new thread in DB
